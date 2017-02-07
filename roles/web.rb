ef783dd7030cfd88effc1bacd9eba8d9b529d554
kkyouhei <hope637@gmail.com>
2017-02-07 23:43:09 +0900

    add git recipe

--- roles/web.rb
+++ roles/web.rb
@@ -1,4 +1,5 @@
 include_recipe "../cookbooks/yum_update/default.rb"
+include_recipe "../cookbooks/git/default.rb"
 include_recipe "../cookbooks/nginx/default.rb"
 include_recipe "../cookbooks/remi/default.rb"
 include_recipe "../cookbooks/php/default.rb"
