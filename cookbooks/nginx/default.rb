9755ffc902b6eabc0e4d1eec99655c73cffeb7be
kkyouhei <hope637@gmail.com>
2017-02-08 11:30:07 +0900

    add nginx default.conf

--- cookbooks/nginx/default.rb
+++ cookbooks/nginx/default.rb
@@ -2,6 +2,15 @@ package "nginx" do
   action :install
 end
 
+NGINX_DEFAULT_CONF = "/etc/nginx/conf.d/default.conf"
+template NGINX_DEFAULT_CONF do
+  owner "root"
+  group "root"
+  mode "644"
+  source "./templates/etc/nginx/conf.d/default.conf.erb"
+  notifies :restart, "service[nginx]"
+end
+
 service "nginx" do
   action [:enable, :start]
 end
