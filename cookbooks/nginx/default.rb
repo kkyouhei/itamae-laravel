898887ad76d870524e41f5f08b912c6ccf6642bb
kkyouhei <hope637@gmail.com>
2017-02-08 13:52:07 +0900

    add example ssl files

--- cookbooks/nginx/default.rb
+++ cookbooks/nginx/default.rb
@@ -10,6 +10,16 @@ remote_file NGINX_LOG_ROTATE do
   source "./files/etc/logrotate.d/nginx"
 end
 
+NGINX_SSL_CERTIFICATE = "/etc/nginx/ssl"
+remote_directory NGINX_LOG_ROTATE do
+  action :create
+  owner "root"
+  group "root"
+  mode "600"
+  path "/etc/nginx/ssl"
+  source "./files/etc/nginx/ssl"
+end
+
 NGINX_DEFAULT_CONF = "/etc/nginx/conf.d/default.conf"
 template NGINX_DEFAULT_CONF do
   owner "root"
